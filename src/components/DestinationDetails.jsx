a0580d4f57647408c858cb19379fdb810785f779
Wasiqur Zaman <wasiqurzaman@gmail.com>
2024-12-12 21:28:35 +0530

    implemant  destination page

--- src/components/DestinationDetails.jsx
+++ src/components/DestinationDetails.jsx
@@ -22,26 +22,26 @@ function DestinationDetails() {
   if (!destination) return null;
 
   return (
-    <div className="flex w-full mx-auto gap-24">
-      <div className="w-1/2">
+    <div className="flex flex-col items-center laptop:flex-row w-full mx-auto gap-8 tablet:gap-12 large-desktop:gap-24">
+      <div className="max-w-1/2 tablet:flex-1 h-full">
         <img
-          src={`/${destination.images.webp}`}
+          src={`../src/assets/${destination.images.webp}`}
           alt={`image of ${destination.name}`}
-          className="w-3/4 mx-auto"
+          className="w-[60%] laptop:w-3/4 mx-auto"
         />
       </div>
-      <div className="w-1/2 flex flex-col gap-6 flex-1  px-4">
+      <div className="flex-1 flex flex-col items-center justify-between laptop:items-start gap-6 tablet:gap-6  large-desktop:gap-12  tablet:px-4 laptop:px-0">
         <DestinationNav destinations={data.destinations} />
-        <h1 className="font-bellefair text-white text-[100px]  tracking-normal leading-[1.146] uppercase ">
+        <h1 className="font-bellefair text-white text-[60px] tablet:text-[80px]  large-desktop:text-[100px]  tracking-normal leading-[1.146] uppercase ">
           {destination.name}
         </h1>
-        <p className="font-barlow text-custom-blue-300 text-lg tracking-normal leading-[1.4]">
+        <p className="tablet:w-[80%] laptop:w-full text-center laptop:text-left font-barlow text-custom-blue-300 tablet:text-lg  desktop:text-lg tracking-normal leading-[1.4] ">
           {" "}
           {destination.description}
         </p>
-        <hr />
-        <div className="flex gap-16">
-          <div className="flex flex-col gap-2">
+        <hr className="h-px w-full bg-gray-200 border-0" />
+        <div className="flex flex-col items-center tablet:flex-row gap-8 tablet:gap-16">
+          <div className="flex flex-col gap-2 text-center tablet:text-left">
             <p className="font-barlow-condensed text-sm text-white tracking-[2px] leading-5 uppercase">
               Avg. Distance
             </p>
@@ -49,7 +49,7 @@ function DestinationDetails() {
               {destination.distance}
             </p>
           </div>
-          <div className="flex flex-col gap-2">
+          <div className="flex flex-col gap-2 text-center tablet:text-left">
             <p className="font-barlow-condensed text-sm text-white tracking-[2px] leading-5 uppercase">
               Est. travel time
             </p>
--- src/pages/Destination.jsx
+++ src/pages/Destination.jsx
@@ -2,9 +2,9 @@ import { Outlet } from "react-router-dom";
 
 function Destination() {
   return (
-    <div className="bg-destination-desktop h-screen bg-cover bg-no-repeat">
-      <div className="max-w-[1280px] mx-auto pt-36 w-3/4 flex flex-col gap-24">
-        <p className="font-barlow-condensed uppercase text-[28px] tracking-[4px] text-white flex gap-4">
+    <div className="bg-destination-mobile tablet:bg-destination-tablet laptop:bg-destination-desktop bg-cover bg-no-repeat pb-40 laptop:pb-0 laptop:h-screen">
+      <div className="max-w-[1280px] mx-auto pt-24 tablet:pt-32 desktop:pt-32 large-desktop:pt-48 w-full px-8  laptop:w-3/4 flex flex-col gap-12 large-desktop:gap-24">
+        <p className="font-barlow-condensed uppercase text-[20px] tablet:text-[24px] laptop:text-[28px] tracking-[4px] text-white flex gap-4 self-center tablet:self-start">
           <span className="text-slate-500 font-semibold">01</span>
           <span>Pick your destination</span>
         </p>
