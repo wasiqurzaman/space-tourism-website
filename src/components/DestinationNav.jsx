40c85203c14706c68ac4932277ea22c080164f44
Wasiqur Zaman <wasiqurzaman@gmail.com>
2024-12-12 21:27:10 +0530

    implemant crew details page

--- src/components/DestinationNav.jsx
+++ src/components/DestinationNav.jsx
@@ -5,12 +5,16 @@ function DestinationNav({ destinations }) {
 
   return (
     <nav className="">
-      <ul className="text-white flex gap-8">
+      <ul className="text-white flex items-center gap-8 h-8">
         {destinations.map(destination => (
           <li key={destination.name}>
             <NavLink
               to={`/destination/${destination.name.toLowerCase()}`}
-              className="font-barlow-condensed tracking-[2px] leading-relaxed uppercase"
+              className={({ isActive }) =>
+                isActive
+                  ? "font-barlow-condensed tracking-[2px] leading-relaxed uppercase border-b-4 py-2 h-full"
+                  : "font-barlow-condensed tracking-[2px] leading-relaxed uppercase hover:border-b-4 hover:border-[#979797] py-2 h-full"
+              }
             >
               {destination.name}
             </NavLink>
