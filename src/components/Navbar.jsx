fc6fab85134735dd993e3fa4257d7748c32044d7
Wasiqur Zaman <wasiqurzaman@gmail.com>
2024-12-12 21:29:00 +0530

    changes iin main navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,9 +4,9 @@ function Navbar() {
   return (
     <nav
       className="large-desktop:max-w-[1400px]
-    flex justify-between mt-4 pr-8 tablet:pr-0 tablet:mt-0 laptop:mt-8 pl-8 laptop:pl-12 w-full items-center absolute translate-x-1/2 right-1/2 text-white "
+    flex justify-between mt-4 pr-8 tablet:pr-0 tablet:mt-0 laptop:mt-4 desktop:mt-8 pl-8 laptop:pl-12 w-full items-center absolute translate-x-1/2 right-1/2 text-white "
     >
-      <img src="shared/logo.svg" alt="logo" className="block" />
+      <img src="../src/assets/shared/logo.svg" alt="logo" className="block" />
 
       <hr className="w-48 desktop:w-80 h-px my-8 bg-gray-200 border-0 dark:bg-gray-700 hidden laptop:block" />
       <ul className="font-barlow-condensed text-xl hidden tablet:flex tablet:items-center tablet:gap-8 lg:gap-16 uppercase bg-slate-900 bg-opacity-55  laptop:pl-24 px-8 py-6 invisible tablet:visible ">
